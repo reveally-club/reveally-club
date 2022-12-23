3911871f0d9a81e05cede71d5fde012b5b8e745a
dilrong <dilrong@dilrong.com>
2022-12-24 03:32:33 +0900

    fix: 빌드 오류 임시 수정

--- src/pages/threads/components/ThreadCard.tsx
+++ src/pages/threads/components/ThreadCard.tsx
@@ -14,7 +14,7 @@ const ThreadCard = (props: ThreadState) => {
       <div className="flex px-4 py-4">
         <article className="w-full ml-4 overflow-auto">
           <div className="flex items-center">
-            <h6 className="text-sm">{shortenAddress(props.author)}</h6>
+            {/* <h6 className="text-sm">{shortenAddress(props.author)}</h6> */}
             <p className="ml-4 text-xs text-stone-500">
               {props.participant}명 참여
             </p>
